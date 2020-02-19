957ae11677849c770c03491a32d2ee5f266ad610
unknown <mikewiercioch@gmail.com>
2020-02-19 23:52:56 +0100

    pipeline test

---
+++ AzureTestApp/Pages/Log.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureTestApp.Pages
+{
+    public class LogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
